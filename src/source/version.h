051fd228bc2b4433a181e704d1d35afcd19a545e
qba667 <jklimasz@gmail.com>
2018-04-05 20:05:26 +0200

    Version incremented

--- src/source/version.h
+++ src/source/version.h
@@ -1,9 +1,10 @@
 #ifndef VERSION_H_
 #define VERSION_H_
 
+uint32_t __attribute__((section (".mod_stack_addr"))) stack_addr = 0x20003990;
 uint16_t __attribute__((section (".mod_version_major"))) version_major = 1;
 uint16_t __attribute__((section (".mod_version_minor"))) version_minor = 0;
 uint16_t __attribute__((section (".mod_version_release"))) version_release = 42;
-uint16_t __attribute__((section (".mod_version_build"))) version_build = 1500;
+uint16_t __attribute__((section (".mod_version_build"))) version_build = 1560;
 
 #endif
